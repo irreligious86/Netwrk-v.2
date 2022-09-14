2a0fee24b6cca2e6c72b0cefdf6b716c487d832a
Oleksandr Voronov <irreligious86@gmail.com>
2022-09-14 13:36:49 -0700

    create profile content

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -8,13 +8,13 @@ const Profile = (props) => {
     return (
         <div className={classes['profile']}>
             <h5 className={classes['profile-title']}>Profile</h5>
-            {/*<ProfileInfo />*/}
-            {/*<MyPosts*/}
-            {/*   postsData={props.profilePage.postsData}*/}
-            {/*   newPostText={props.profilePage.newPostText}*/}
-            {/*   addPost={props.addPost}*/}
-            {/*   updateNewPostText={props.updateNewPostText}*/}
-            {/*/>*/}
+            <ProfileInfo />
+            <MyPosts
+               // postsData={props.profilePage.postsData}
+               // newPostText={props.profilePage.newPostText}
+               // addPost={props.addPost}
+               // updateNewPostText={props.updateNewPostText}
+            />
         </div>
     )
 }
