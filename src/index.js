cdab0cc901ca3f02135f2f053a4cbbc7a2060603
Oleksandr Voronov <irreligious86@gmail.com>
2022-09-14 12:09:46 -0700

    init props and import state in index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
 import './index.css';
+import { addPost, updateNewPostText } from './redux/state';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+// import { subscribe } from './redux/state';
+import state from './redux/state.js';
 
 const rerenderEntireTree = () => {
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <BrowserRouter>
             <App
-                // state={state}
-                // addPost={addPost}
-                // updateNewPostText={updateNewPostText}
+                state={state}
+                addPost={addPost}
+                updateNewPostText={updateNewPostText}
             />
         </BrowserRouter>
     );
 };
 
 rerenderEntireTree();
+
+// subscribe(rerenderEntireTree);
