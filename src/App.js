1eed648d051080b1f819f1b460e8536abccc95d6
Oleksandr Voronov <irreligious86@gmail.com>
2022-09-14 13:07:29 -0700

    init BrowserRouter, Routes, and set up navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import { Routes, Route} from "react-router-dom";
 import './App.css';
 import Header from './components/header/header';
 import Nav from './components/nav/nav';
-// import Profile from './components/profile/profile';
-// import Dialogs from './components/dialogs/dialogs';
-// import Settings from './components/settings/settings';
-// import Music from './components/music/music';
-// import News from './components/news/news';
+import Profile from './components/profile/profile';
+import Dialogs from './components/dialogs/dialogs';
+import Settings from './components/settings/settings';
+import Music from './components/music/music';
+import News from './components/news/news';
 import Footer from './components/footer/footer';
 
 const App = (props) => {
-
   return (
       <div className='app-wrapper'>
         <Header />
         <Nav />
-        <div className='app-content'></div>
+        <div className='app-content'>
+            <Routes>
+                <Route
+                    path="/profile"
+                    element={<Profile
+                        // profilePage={props.state.profilePage}
+                        // addPost={props.addPost}
+                        // updateNewPostText={props.updateNewPostText}
+                    />}
+                />
+                <Route
+                    path="/dialogs"
+                    element={<Dialogs
+                        // dialogsPage={props.state.dialogsPage}
+                    />}
+                />
+                <Route path="/news" element={<News/>}/>
+                <Route path="/music" element={<Music/>}/>
+                <Route path="/settings" element={<Settings/>}/>
+            </Routes>
+        </div>
         <Footer />
       </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 const rerenderEntireTree = () => {
-
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
+        <BrowserRouter>
             <App
                 // state={state}
                 // addPost={addPost}
                 // updateNewPostText={updateNewPostText}
             />
+        </BrowserRouter>
     );
 };
 
