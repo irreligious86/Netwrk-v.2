923f567d4f273840845e950c1c8e2d84b36d64ac
Oleksandr Voronov <irreligious86@gmail.com>
2022-09-16 07:27:24 -0700

    fix problem with sidebar items visible

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -5,43 +5,43 @@ import classes from './nav.module.css';
 const Nav = (props) => {
     return (
         <div className={classes.nav}>
-            {/*<ul className={classes['nav-list']}>*/}
-            {/*    <li className={classes['nav-list-item']}>*/}
-            {/*        <NavLink*/}
-            {/*            to="/profile"*/}
-            {/*            className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>*/}
-            {/*            Profile*/}
-            {/*        </NavLink>*/}
-            {/*    </li>*/}
-            {/*    <li className={classes['nav-list-item']}>*/}
-            {/*        <NavLink*/}
-            {/*            to="/dialogs"*/}
-            {/*            className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>*/}
-            {/*            Dialogs*/}
-            {/*        </NavLink>*/}
-            {/*    </li>*/}
-            {/*    <li className={classes['nav-list-item']}>*/}
-            {/*        <NavLink*/}
-            {/*            to="/news"*/}
-            {/*            className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>*/}
-            {/*            News*/}
-            {/*        </NavLink>*/}
-            {/*    </li>*/}
-            {/*    <li className={classes['nav-list-item']}>*/}
-            {/*        <NavLink*/}
-            {/*            to="/music"*/}
-            {/*            className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>*/}
-            {/*            Music*/}
-            {/*        </NavLink>*/}
-            {/*    </li>*/}
-            {/*    <li className={classes['nav-list-item']}>*/}
-            {/*        <NavLink*/}
-            {/*            to="/settings"*/}
-            {/*            className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>*/}
-            {/*            Settings*/}
-            {/*        </NavLink>*/}
-            {/*    </li>*/}
-            {/*</ul>*/}
+            <ul className={classes['nav-list']}>
+                <li className={classes['nav-list-item']}>
+                    <NavLink
+                        to="/profile"
+                        className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>
+                        Profile
+                    </NavLink>
+                </li>
+                <li className={classes['nav-list-item']}>
+                    <NavLink
+                        to="/dialogs"
+                        className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>
+                        Dialogs
+                    </NavLink>
+                </li>
+                <li className={classes['nav-list-item']}>
+                    <NavLink
+                        to="/news"
+                        className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>
+                        News
+                    </NavLink>
+                </li>
+                <li className={classes['nav-list-item']}>
+                    <NavLink
+                        to="/music"
+                        className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>
+                        Music
+                    </NavLink>
+                </li>
+                <li className={classes['nav-list-item']}>
+                    <NavLink
+                        to="/settings"
+                        className = { navData => navData.isActive ? classes['nav-link-active'] : classes['nav-link'] }>
+                        Settings
+                    </NavLink>
+                </li>
+            </ul>
         </div>
     )
 }
