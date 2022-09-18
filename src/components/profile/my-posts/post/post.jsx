97cbfdfa0385926cd063b54b7f24e788895d7df2
irreligious86 <irreligious86@gmail.com>
2022-09-18 14:33:53 +0300

    make refactoring post

--- src/components/profile/my-posts/post/post.jsx
+++ src/components/profile/my-posts/post/post.jsx
@@ -10,12 +10,18 @@ const Post = (props) => {
                     <p className={classes['post-author-name']}>Author name</p>
                     <p className={classes['post-author-rating']}>Author rating</p>
                 </div>
-                <div className={classes['post-content']}>
-                    <h5 className={classes['post-title']}>Post title</h5>
-                    <p className={classes['post-message']}>{props.message}</p>
-                    <p className={classes['post-likes']}>Like: {props.likesCount}</p>
-                    <p className={classes['post-date']}>22-04-2020</p>
+                {/*<div className={classes['post-content']}>*/}
+                <h5 className={classes['post-title']}>Post title</h5>
+                <p className={classes['post-message']}>{props.message}</p>
+                <div className={classes['post-controls']}>
+                    <button className={classes['post-controls-btn']}
+                    type='button'>Button 1</button>
+                    <button className={classes['post-controls-btn']}
+                    type='button'>Button 2</button>
                 </div>
+                <span className={classes['post-likes']}>Like: {props.likesCount}</span>
+                <span className={classes['post-date']}>22-04-2020</span>
+                {/*</div>*/}
             </div>
         </div>
     )
